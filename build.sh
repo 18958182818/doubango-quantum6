531b744c959096eab414f4bd15319d97ba831164
quantum6 <quantum6@yeah.net>
2020-02-23 17:16:02 +0800

    增加g729编译参数。

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ rm config.log
     CFLAGS=-I${BUILD_LIBS}/include \
     LDFLAGS=-L${BUILD_LIBS}/lib \
     LIBS="-lm -lc -lswresample -lavutil -lyuv -ljpeg " \
-    --with-ssl --with-srtp --with-vpx --with-yuv --with-amr --with-speex --with-speexdsp --enable-speexresampler --enable-speexdenoiser --with-opus --with-gsm --with-ilbc --with-ffmpeg
+    --with-ssl --with-srtp --with-vpx --with-yuv --with-amr --with-speex --with-speexdsp --enable-speexresampler --enable-speexdenoiser --with-opus --with-gsm --with-ilbc --with-g729 --with-ffmpeg
  
 # --with-g729
 #    --with-speexdsp --with-ffmpeg --with-opus
